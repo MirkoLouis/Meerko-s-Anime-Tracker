abc24035beca48c50857e0c4d189c01ca9ae6ab8
MirkoLouis <mirkolouis33@gmail.com>
2025-12-05 15:29:37 +0800

    feat: Add SQL scripts for database creation, user inserts, and spotlight updates

---
+++ auto insert to db/user_inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO `user` (`username`, `userpassword`, `display_name`, `email`) VALUES
+('mirkolouis', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'MirkoLouis', 'admin@example.com'),
+('j_doe', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG3', 'John Doe', 'j.doe@testmail.com'),
+('jane_dev', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Jane Developer', 'jane.dev@techlabs.io'),
+('code_ninja', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Code Ninja', 'ninja@coding.net'),
+('frontend_fan', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Frontend Fanatic', 'ui@frontend.org'),
+('backend_guru', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Backend Guru', 'api@backend.com'),
+('qa_tester', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'QA Tester', 'bugs@quality.co'),
+('guest_user', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Guest Account', 'guest@tempmail.net'),
+('data_wizard', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Data Wizard', 'db@analytics.io'),
+('design_pro', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Design Pro', 'ux@creative.studio');
