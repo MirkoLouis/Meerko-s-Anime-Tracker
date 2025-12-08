0ca787c8fc98d2c885daa67a738f7f1dfd958cf0
MirkoLouis <mirkolouis33@gmail.com>
2025-12-09 06:55:21 +0800

    Corrected a mistake in insert_users where j_doe could not login using 123 because there was a 3 in the password.

--- auto insert to db/insert_users.sql
+++ auto insert to db/insert_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO `user` (`username`, `userpassword`, `display_name`, `email`, `role`) VALUES
 ('mirkolouis', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'MirkoLouis', 'admin@example.com', 'admin'),
-('j_doe', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG3', 'John Doe', 'j.doe@testmail.com', 'user'),
+('j_doe', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'John Doe', 'j.doe@testmail.com', 'user'),
 ('jane_dev', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Jane Developer', 'jane.dev@techlabs.io', 'user'),
 ('code_ninja', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Code Ninja', 'ninja@coding.net', 'user'),
 ('frontend_fan', '$2b$08$da9QlHTFH7Gkuufy8BVDYuf3TzuRRnwkk5PoHyDMNmH1VZumFr6JG', 'Frontend Fanatic', 'ui@frontend.org', 'user'),
